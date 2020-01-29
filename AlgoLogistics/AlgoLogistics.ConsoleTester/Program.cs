7f8f2c8eac0e4e987a6253e91d4038aa4724a339
Romaniv Volodymyr <romaniv.volodumur@gmail.com>
2020-01-29 23:15:44 +0200

    Added basic algorithm interface and decorator for measuring execution time

--- AlgoLogistics/AlgoLogistics.ConsoleTester/Program.cs
+++ AlgoLogistics/AlgoLogistics.ConsoleTester/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using AlgoLogistics.Algorithms;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AlgoLogistics.ConsoleTester
 {
+	class A : IAlgorithm<int, int>
+	{
+		public int Execute(int input)
+		{
+			Thread.Sleep(1500);
+
+			return input;
+		}
+	}
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var alg = new MeasurableAlgorithm<int, int>(new A());
+
+			var result = alg.Execute(5);
+
+			Console.WriteLine(result);
 		}
 	}
 }
