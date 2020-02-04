06ac1805a2a16dcb00138f40ba68e1a94d18764c
Romaniv Volodymyr <romaniv.volodumur@gmail.com>
2020-02-04 22:48:20 +0200

    Added logic to entities

---
+++ AlgoLogistics/AlgoLogistics.Domain/Entities/Measure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoLogistics.Domain.Entities
+{
+	public class Measure
+	{
+		public const double MaxDimensionValueInMetres = 1.5;
+		public double Width { get; set; }
+		public double Height { get; set; }
+		public double Length { get; set; }
+
+		public Measure(double width, double height, double length)
+		{
+			if (width <= 0 || height <= 0 || length <= 0)
+			{
+				throw new ArgumentException("Dimensions cannot be negative");
+			}
+
+			if (width > MaxDimensionValueInMetres || height > MaxDimensionValueInMetres || length > MaxDimensionValueInMetres)
+			{
+				throw new ArgumentException($"Max dimension value is {MaxDimensionValueInMetres} metre(s)");
+			}
+
+			Width = width;
+			Height = height;
+			Length = length;
+		}
+	}
+}
--- AlgoLogistics/AlgoLogistics.Domain/Entities/Package.cs
+++ AlgoLogistics/AlgoLogistics.Domain/Entities/Package.cs
@@ -8,15 +8,125 @@ namespace AlgoLogistics.Domain.Entities
 {
 	public class Package : AuditableEntity
 	{
-		public int Id { get; set; }
-		public string Description { get; set; }
-		public decimal Price { get; set; }
-		public WeightCategory Weight { get; set; }
-		public SizeCategory Size { get; set; }
-		public string Sender { get; set; }
-		public string Reciever { get; set; }
-		public string FromCity { get; set; }
-		public string ToCity { get; set; }
-		public DeliveryStatus Status { get; set; }
+		public int Id { get; private set; }
+		public string Description { get; private set; }
+		public decimal Price { get; private set; }
+		public WeightCategory Weight { get; private set; }
+		public SizeCategory Size { get; private set; }
+		public Measure Measure { get; private set; }
+		public string Sender { get; private set; }
+		public string Receiver { get; private set; }
+		public string FromCity { get; private set; }
+		public string ToCity { get; private set; }
+		public DeliveryStatus Status { get; private set; }
+
+		private Package() { }
+
+		public Package(string description, Measure measure, double weight, string sender, string receiver, string fromCity, string toCity)
+		{
+			if (!IsValidInput(description, measure, weight, sender, receiver, fromCity, toCity, out ArgumentException exception))
+			{
+				throw exception;
+			}
+
+			Description = description;
+			Sender = sender;
+			Receiver = receiver;
+			FromCity = fromCity;
+			ToCity = toCity;
+			Status = DeliveryStatus.NotSent;
+			Weight = GetWeightCategory(weight);
+			Size = GetSizeCategory(measure);
+			Price = CalculatePrice();
+		}
+
+		private decimal CalculatePrice()
+		{
+			var price = 0.0m;
+
+			return price;
+		}
+
+		private SizeCategory GetSizeCategory(Measure measure)
+		{
+			var volume = measure.Height * measure.Length * measure.Width;
+
+			if (volume > 0 && volume <= 0.01)
+			{
+				return SizeCategory.Small;
+			}
+			else if (volume > 0.01 && volume <= 0.07)
+			{
+				return SizeCategory.Medium;
+			}
+			else if (volume > 0.07 && volume <= 1)
+			{
+				return SizeCategory.Large;
+			}
+			else
+			{
+				return SizeCategory.ExtraLarge;
+			}
+		}
+
+		private WeightCategory GetWeightCategory(double weight)
+		{
+			if (weight > 0 && weight <= 5)
+			{
+				return WeightCategory.Light;
+			} 
+			else if (weight > 5 && weight <= 10)
+			{
+				return WeightCategory.Medium;
+			}
+			else
+			{
+				return WeightCategory.Heavy;
+			}
+		}
+
+		private bool IsValidInput(string description, Measure measure, double weight, string sender, string receiver, string fromCity, string toCity, out ArgumentException exception)
+		{
+			bool result = false;
+			exception = null;
+
+			if (string.IsNullOrEmpty(description))
+			{
+				exception = new ArgumentException($"{nameof(description)} cannot be null or empty", nameof(description));
+				return result;
+			} 
+			else if (measure == null)
+			{
+				exception = new ArgumentException($"{nameof(measure)} cannot be null", nameof(measure));
+				return result;
+			}
+			else if (weight <= 0)
+			{
+				exception = new ArgumentException($"{nameof(weight)} should be greater than zero", nameof(weight));
+				return result;
+			}
+			else if (string.IsNullOrEmpty(sender))
+			{
+				exception = new ArgumentException($"{nameof(sender)} cannot be null or empty", nameof(sender));
+				return result;
+			}
+			else if (string.IsNullOrEmpty(receiver))
+			{
+				exception = new ArgumentException($"{nameof(receiver)} cannot be null or empty", nameof(receiver));
+				return result;
+			}
+			else if (string.IsNullOrEmpty(fromCity))
+			{
+				exception = new ArgumentException($"{nameof(fromCity)} cannot be null or empty", nameof(fromCity));
+				return result;
+			}
+			else if (string.IsNullOrEmpty(toCity))
+			{
+				exception = new ArgumentException($"{nameof(toCity)} cannot be null or empty", nameof(toCity));
+				return result;
+			}
+
+			return true;
+		}
 	}
 }
